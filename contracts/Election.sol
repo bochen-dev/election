6908c251645aa40358e7d6f45dbd0afef6350fc7
bochen-chen <code.bochen@gmail.com>
2018-08-11 20:07:41 +0800

    3. Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,9 +8,13 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping (address => bool) public voters;
+
     // Store Candidates
     // Fetch Candidates
     mapping (uint => Candidate) public candidates;
+    
     // Store Candidates Count
     uint public candidatesCount;
 
@@ -22,5 +26,19 @@ contract Election {
     function addCandidate(string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public {
+        // require that they haven't vote before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount ++;
     }
 }
